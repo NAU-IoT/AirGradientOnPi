9ac46590836dbfe0992c8ba3f6a157f016e7f35c
Michael Chestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-07-28 13:15:52 -0700

    Update TestAirQuality.py

--- TestAirQuality.py
+++ TestAirQuality.py
@@ -5,59 +5,71 @@ from PIL import ImageFont
 import smbus
 import time
 
-while True:
+
+# Get I2C bus for SHT30
+bus = smbus.SMBus(1)
+
+# Raspberry Pi pin configuration:
+RST = 24
+
+# Initialize OLED display
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C, i2c_bus=1)
+disp.begin()
+disp.clear()
+disp.display()
+
+# Load font
+font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
+# Create image buffer
+image = Image.new('1', (disp.width, disp.height))
+draw = ImageDraw.Draw(image)
+
+try:
+    while True:
     
-    # Get I2C bus for SHT30
-    bus = smbus.SMBus(1)
-    
-    # SHT30 address, 0x44(68)
+        # SHT30 address, 0x44(68)
         # Send measurement command, 0x2C(44)
         #           0x06(06)        High repeatability measurement
-    bus.write_i2c_block_data(0x44, 0x2C, [0x06])
-    time.sleep(0.5)
+        bus.write_i2c_block_data(0x44, 0x2C, [0x06])
+        time.sleep(0.5)
         # SHT30 address, 0x44(68)
         # Read data back from 0x00(00), 6 bytes
         # cTemp MSB, cTemp LSB, cTemp CRC, Humididty MSB, Humidity LSB, Humidity CRC
-    data = bus.read_i2c_block_data(0x44, 0x00, 6)
+        data = bus.read_i2c_block_data(0x44, 0x00, 6)
         # Calculate the data
-    cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45
-    fTemp = cTemp * 1.8 + 32
-    humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+        cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45
+        fTemp = cTemp * 1.8 + 32
+        humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
         # Output data to screen
-    print("Relative Humidity : %.2f %%RH" %humidity)
+        print("Relative Humidity : %.2f %%RH" %humidity)
         #print("Temperature in Celsius : %.2f C" %cTemp)
-    print("Temperature in Fahrenheit : %.2f F" %fTemp)
-    
-    
-    # Raspberry Pi pin configuration:
-    RST = 24
-    
-    # Initialize OLED display
-    disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C, i2c_bus=1)
-    disp.begin()
-    disp.clear()
-    disp.display()
-    
-    # Create image buffer
-    image = Image.new('1', (disp.width, disp.height))
-    draw = ImageDraw.Draw(image)
-    
-    # Load font
-    font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
-    
-    # Display temperature and humidity on OLED
-    temp = "Temp: "
-    tempval = ("%.2f F" %fTemp)
-    hum = "Humidity: "
-    humval = ("%.2f %%RH" %humidity)
-    
-    draw.text((32, 15), temp, font=font, fill=255)
-    draw.text((32, 25), tempval, font=font, fill=255)
-    draw.text((32, 40), hum, font=font, fill=255)
-    draw.text((32, 50), humval, font=font, fill=255)
-    
-    # Show the image on the display
-    disp.image(image)
-    disp.display()
-    
-    time.sleep(2)
+        print("Temperature in Fahrenheit : %.2f F" %fTemp)
+        
+        # Clear display
+        disp.clear()
+        disp.display()
+        
+        # Display temperature and humidity on OLED
+        temp = "Temp: "
+        tempval = ("%.2f F" %fTemp)
+        hum = "Humidity: "
+        humval = ("%.2f %%RH" %humidity)
+        
+        draw.text((32, 15), temp, font=font, fill=255)
+        draw.text((32, 25), tempval, font=font, fill=255)
+        draw.text((32, 40), hum, font=font, fill=255)
+        draw.text((32, 50), humval, font=font, fill=255)
+        
+        # Show the image on the display
+        disp.image(image)
+        disp.display()
+        
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    # Exit the loop if Ctrl+C is pressed
+    pass
+
+# Clear the display before exiting the script
+disp.clear()
+disp.display()
