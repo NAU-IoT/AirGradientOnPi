4979c5d1cfe91de3e883358589a712d7a62f8528
Michael Chestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-07-28 15:38:13 -0700

    Update TestAirQuality.py

--- TestAirQuality.py
+++ TestAirQuality.py
@@ -1,99 +1,71 @@
-#import libraries
-import network
-import TAQconfiguration as config
+import Adafruit_SSD1306
 import time
-from senseair_s8 import SenseairS8
-import smbus
 import board
 import busio
-from digitalio import DigitalInOut, Direction, Pull
-from adafruit_pm25.i2c import PM25_I2C
-import serial
-
-#initialize variables from config file
-SSID = config.ssid
-PASSWORD = config.password
-TEMP_SCALE = config.temp_scale
-
-# connect the pi to the internet
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(SSID, PASSWORD)
-print(wlan.isconnected())
+import adafruit_scd30
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 
-uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
-# Create library object, use 'slow' 100KHz frequency!
-i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
-# Connect to a PM2.5 sensor over I2C
-pm25 = PM25_I2C(i2c, reset_pin)
-
-print("Found PM2.5 sensor, reading data...")
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
 
 
+# SCD-30 has tempremental I2C with clock stretching, datasheet recommends
+# starting at 50KHz
+i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
+scd = adafruit_scd30.SCD30(i2c)
 
-while(True)
-# Senseair s8
-    senseair_s8 = SenseairS8() 
-    # get CO2 value
-    CO2 = senseair_s8.co2()
+# Raspberry Pi pin configuration:
+RST = 24
 
-# SHT30
-    # Get I2C bus
-    bus = smbus.SMBus(1)
-    # SHT30 address, 0x44(68)
-    # Send measurement command, 0x2C(44)
-    #		0x06(06)	High repeatability measurement
-    bus.write_i2c_block_data(0x44, 0x2C, [0x06])
-    time.sleep(0.5)
-    # SHT30 address, 0x44(68)
-    # Read data back from 0x00(00), 6 bytes
-    # cTemp MSB, cTemp LSB, cTemp CRC, Humididty MSB, Humidity LSB, Humidity CRC
-    data = bus.read_i2c_block_data(0x44, 0x00, 6)
-    # Calculate the data
-    cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45
-    fTemp = cTemp * 1.8 + 32
-    humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+# Initialize OLED display
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C, i2c_bus=1)
+disp.begin()
+disp.clear()
+disp.display()
 
-# PM2.5
-    reset_pin = None
-    # If you have a GPIO, its not a bad idea to connect it to the RESET pin
-    # reset_pin = DigitalInOut(board.G0)    
-    # reset_pin.direction = Direction.OUTPUT
-    # reset_pin.value = False
-
-    time.sleep(1)
-
-    try:
-        aqdata = pm25.read()
-        # print(aqdata)
-    except RuntimeError:
-        print("Unable to read from sensor, retrying...")
-        continue
-
-    print()
-    print("Concentration Units (standard)")
-    print("---------------------------------------")
-    print(
-        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-        % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
-    )
-    print("Concentration Units (environmental)")
-    print("---------------------------------------")
-    print(
-        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-        % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
-    )
-    print("---------------------------------------")
-    print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
-    print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
-    print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
-    print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
-    print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
-    print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
-    print("---------------------------------------")
-    
-    
+# Load font
+font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
 
 
-   
+
+while True:
+    # since the measurement interval is long (2+ seconds) we check for new data before reading
+    # the values, to ensure current readings.
+    if scd.data_available:
+        print("Data Available!")
+        print("CO2: %d PPM" % scd.CO2)
+        cTemp = scd.temperature
+        fTemp = (cTemp * 9/5) + 32
+        humidity = scd.relative_humidity
+        print("Temperature in Fahrenheit : %.2f F" %fTemp)
+        print("Relative Humidity : %.2f %%RH" %humidity)
+        print("")
+        print("Waiting for new data...")
+        print("")
+
+    # Create image buffer
+    image = Image.new('1', (disp.width, disp.height))
+    draw = ImageDraw.Draw(image)
+
+    # Display temperature and humidity on OLED
+    temp = "Temp: "
+    tempval = ("%.2f F" %fTemp)
+    hum = "Humidity: "
+    humval = ("%.2f %%RH" %humidity)
+
+    draw.text((32, 15), temp, font=font, fill=255)
+    draw.text((32, 25), tempval, font=font, fill=255)
+    draw.text((32, 40), hum, font=font, fill=255)
+    draw.text((32, 50), humval, font=font, fill=255)
+        
+    # Show the image on the display
+    disp.clear()
+    disp.image(image)
+    disp.display()
+        
+    time.sleep(2)
+
