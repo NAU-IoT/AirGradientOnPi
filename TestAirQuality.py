66745c7fc14a3a409c30009ec9d52be31eff4ca6
Michael Chestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-08-21 17:15:37 -0700

    Update TestAirQuality.py

--- TestAirQuality.py
+++ TestAirQuality.py
@@ -14,56 +14,66 @@ def celsius_to_fahrenheit(celsius):
     fahrenheit = (celsius * 9/5) + 32
     return fahrenheit
 
-#        SCD-30ˇˇˇˇˇˇ
-# SCD-30 has tempremental I2C with clock stretching, datasheet recommends
-# starting at 50KHz
-i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
-scd = adafruit_scd30.SCD30(i2c)
 
-# Raspberry Pi pin configuration:
-RST = 24
-
-#        OLEDˇˇˇˇˇˇ
-# Initialize OLED display
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C, i2c_bus=1)
-disp.begin()
-disp.clear()
-disp.display()
-
-# Load font
-font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
-
-#        PM2.5ˇˇˇˇˇ
-# Define the serial port and baud rate
-serial_port = "/dev/ttyS0"
-baud_rate = 9600
-
-# Initialize the serial connection
-uart = serial.Serial(serial_port, baudrate=baud_rate, timeout=0.25)
-
-# Initialize the PM2.5 sensor over UART
-pm25 = PM25_UART(uart)
+def scd30_initialization():
+    # SCD-30 has tempremental I2C with clock stretching, datasheet recommends
+    # starting at 50KHz
+    i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
+    scd = adafruit_scd30.SCD30(i2c)
+    # Raspberry Pi pin configuration:
+    RST = 24
 
 
-while True:
-    # since the measurement interval is long (2+ seconds) we check for new data before reading
-    # the values, to ensure current readings.
-    if scd.data_available:
-        print("Data Available!")
-        print("CO2: %d PPM" % scd.CO2)
-        cTemp = scd.temperature
-        fTemp = (cTemp * 9/5) + 32
-        co2ppm = scd.CO2
-        humidity = scd.relative_humidity
-        print("Temperature in Fahrenheit : %.2f F" %fTemp)
-        print("Relative Humidity : %.2f %%RH" %humidity)
-        print("")
-        print("Waiting for new data...")
-        print("")
+def oled_initialization():
+    # Initialize OLED display
+    disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C, i2c_bus=1)
+    disp.begin()
+    disp.clear()
+    disp.display()
+    # Load font
+    font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
 
+
+def pmsensor_initialization():
+    # Define the serial port and baud rate
+    serial_port = "/dev/ttyS0"
+    baud_rate = 9600
+    # Initialize the serial connection
+    uart = serial.Serial(serial_port, baudrate=baud_rate, timeout=0.25)
+    # Initialize the PM2.5 sensor over UART
+    pm25 = PM25_UART(uart)
+
+def create_image_buffer(disp_width, disp_height):
     # Create image buffer
-    image = Image.new('1', (disp.width, disp.height))
+    image = Image.new('1', (disp_width, disp_height))
     draw = ImageDraw.Draw(image)
+    return image, draw
+
+
+def main():
+    scd30_initialization()
+
+    oled_initialization()
+
+    pmsensor_initialization()
+
+
+    while True:
+        if scd.data_available:
+            #print("Data Available!")
+            #print("CO2: %d PPM" % scd.CO2)
+            cTemp = scd.temperature
+            fTemp = (cTemp * 9/5) + 32
+            co2ppm = scd.CO2
+            humidity = scd.relative_humidity
+            #print("Temperature in Fahrenheit : %.2f F" %fTemp)
+            #print("Relative Humidity : %.2f %%RH" %humidity)
+            #print("")
+            #print("Waiting for new data...")
+            #print("")
+
+        # Create image buffer
+        image, draw = create_image_buffer(disp.width, disp.height)
 
     # Assign values to variables
     temp = "Temp: "
